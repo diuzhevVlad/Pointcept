cff41d44eee8b66972f2529edb0f7dc58164c7f6
Vladislav Diuzhev <vladdiuzhev@gmail.com>
2025-03-13 10:17:46 +0300

    run script added

---
+++ scripts/run_container.sh
@@ -0,0 +1,14 @@
+DATASET_PATH=/home/vladislav/Documents/SemanticKITTI
+IMG_TAG=pointcept/pointcept:pytorch2.0.1-cuda11.7-cudnn8-devel
+
+docker run \
+  -it \
+  --rm \
+  --net=host \
+  --ipc=host \
+  --pid=host \
+  --gpus all \
+  -e=DISPLAY \
+  -v ${DATASET_PATH}:/workspace/Pointcept/data/semantic_kitti/ \
+  ${IMG_TAG} \
+  zsh
